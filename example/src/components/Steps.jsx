8398b145075ce4f45dbb44b2efa229501e10a500
joshuaasare <joshuaasare29@gmail.com>
2020-05-02 21:20:54 +0000

    added animations

---
+++ example/src/components/Steps.jsx
@@ -0,0 +1,22 @@
+/*
+ * @Author: Joshua Asare
+ * @Date: 2020-05-02 18:33:45
+ * @Last Modified by: Joshua Asare
+ * @Last Modified time: 2020-05-02 18:34:59
+ */
+
+import React from 'react';
+import './css/steps.css';
+import { Carousel } from 'react-circular-carousel';
+
+const Steps = () => {
+  return (
+    <div className='steps'>
+      <Carousel width={300} height={250} id={2}>
+        {}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Steps;
--- example/src/components/index.js
+++ example/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Landing } from './Landing';
 export { default as Cards } from './Cards';
 export { default as Images } from './Images';
+export { default as Steps } from './Steps';
